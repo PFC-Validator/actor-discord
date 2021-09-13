bea4528c6d10cfeb20c54f9b4c61cdfaf0a466f8
PFC(Ian) <pfc-validator@protonmail.com>
2021-09-13 10:00:19 -0500

    move discordbot

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use actix_broker::SystemBroker;
 
 mod api;
-pub mod connection;
+mod connection;
 pub mod discord;
 mod errors;
 mod intents;
 pub mod types;
 pub use api::DiscordAPI;
+pub use connection::DiscordBot;
 pub use intents::GatewayIntents;
 /// VERSION number of package
 pub const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
